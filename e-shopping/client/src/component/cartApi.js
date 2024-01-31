7443648729df2a3772038fb55b10dd5ea24c4c99
AlgoMean143 <108215426+Mihir069@users.noreply.github.com>
2024-01-31 16:59:19 +0530

    commit change

--- e-shopping/client/src/component/cartApi.js
+++ e-shopping/client/src/component/cartApi.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 
 const   CartApi = () =>{
     const [cartItems,setCartItems] = useState([]);
-    const [isItemInCart,setIsItemIncart] = useState({})
+    const [isItemInCart,setIsItemIncart] = useState({});
+
+    useEffect(()=>{
+        fetch(`http://localhost:3001/get-cart`)
+            .then((res)=>res.json())
+            .then((data)=>{
+                setCartItems(data);
+                const itemInCart = {};
+                data.forEach((item)=>{
+                    itemInCart[item.id] = true;
+                });
+                setIsItemIncart(itemInCart);
+            })
+            .catch((error)=>{
+                console.error('Error in cart :',error)
+            })
+    },[])
 }
 export default CartApi;
